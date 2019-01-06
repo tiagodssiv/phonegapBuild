3ee09ab2a25491d77b695396abe2843222392003
Tiago SILVA <tiagodssiv@yahoo.com.br>
2019-01-06 03:28:30 -0200

    teste 5 mudana de arquivos

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -21,6 +21,14 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+
+
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    alert(device.model);
+}
+,
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -28,13 +36,17 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+ 
+
+
+
+
+        
+    
+
+,
+
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
